16689e1e07b6511d6f3a6214f29f8a5a955ab5ea
Valeria <05valarist@gmail.com>
2023-09-13 19:56:47 -0500

    clases del modelo, clase conexion

---
+++ trabajoProductoValDani/src/modelo/Producto.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author val
+ */
+public class Producto {
+    private int codigo;
+    private String nombre;
+    private int precio;
+    private String distribuidor;
+    private int idCategoria;
+
+    public Producto(int codigo, String nombre, int precio, String distribuidor, int idCategoria) {
+        this.codigo = codigo;
+        this.nombre = nombre;  
+        this.precio = precio;
+        this.distribuidor = distribuidor;
+        this.idCategoria = idCategoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getDistribuidor() {
+        return distribuidor;
+    }
+
+    public void setDistribuidor(String distribuidor) {
+        this.distribuidor = distribuidor;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+    
+    
+    
+    
+}
