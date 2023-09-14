0d674de2c1432c9149dd641defc622343f187456
Danistan015 <sotograjalesdaniela@gmail.com>
2023-09-13 19:56:17 -0500

    miniAvancesx2

--- trabajoProductoValDani/src/Vista/Menu.java
+++ trabajoProductoValDani/src/Vista/Menu.java
@@ -39,6 +39,11 @@ public class Menu extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Poppins Medium", 0, 14)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 229, 236));
         jButton1.setText("Gestion categorias");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setBackground(new java.awt.Color(251, 111, 146));
         jButton2.setFont(new java.awt.Font("Poppins Medium", 0, 14)); // NOI18N
@@ -94,6 +99,12 @@ public class Menu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        new GestionCategoria().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
