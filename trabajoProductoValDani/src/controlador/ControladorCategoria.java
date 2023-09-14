9affc596596d5b7e868abbc1e0c5279a5c2f27fc
Valeria <05valarist@gmail.com>
2023-09-13 23:00:07 -0500

    métodos categorías en dao y controlador

---
+++ trabajoProductoValDani/src/controlador/ControladorCategoria.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import dao.DaoCategorias;
+import java.sql.SQLException;
+import modelo.Categoria;
+
+/**
+ *
+ * @author val
+ */
+public class ControladorCategoria {
+    DaoCategorias dao;
+
+    public ControladorCategoria() {
+        dao = new DaoCategorias();
+    }
+    
+    public void agregarCategoria(Categoria categoria) throws SQLException{
+        dao.agregarCategoria(categoria);
+    }
+    
+    public Categoria buscarCategoria(int id) throws SQLException{
+        return dao.buscarCategoria(id);
+    }
+    
+    public Categoria editarCategoria(int id) throws SQLException{
+        return dao.editarCategoria(id);
+    }
+    
+    public void eliminarCategoria(int id) throws SQLException{
+        dao.eliminarCategoria(id);
+    }
+}
--- trabajoProductoValDani/src/dao/DaoCategorias.java
+++ trabajoProductoValDani/src/dao/DaoCategorias.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.Categoria;
 
 /**
@@ -20,7 +22,6 @@ public class DaoCategorias {
     public void agregarCategoria(Categoria categoria) throws SQLException {
         try {
             PreparedStatement ps = null;
-            ResultSet rs = null;
             Conexion_db conn = new Conexion_db();
             Connection con = conn.getConexion();
 
@@ -30,6 +31,66 @@ public class DaoCategorias {
             ps.setString(2, categoria.getNombre());
             ps.execute();
 
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            throw new SQLException();
+        }
+    }
+
+    public Categoria buscarCategoria(int id) throws SQLException {
+        Categoria categoriaEncontrada = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Conexion_db conn = new Conexion_db();
+        Connection con = conn.getConexion();
+
+        String where = " WHERE id = '" + id + "'";
+        String sql = "SELECT * FROM categorias" + where;
+
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                categoriaEncontrada = new Categoria(id, nombre);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            throw new SQLException();
+        }
+
+        return categoriaEncontrada;
+    }
+
+    public Categoria editarCategoria(int id) throws SQLException {
+        Categoria categoriaEncontrada = buscarCategoria(id);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Conexion_db obConexion_db = new Conexion_db();
+        Connection conn = obConexion_db.getConexion();
+        try {
+            ps = conn.prepareStatement("UPDATE  categoria SET nombre=? WHERE id= '" + id + "'");
+            String nombre = rs.getString("nombre");
+            categoriaEncontrada.setNombre(nombre);
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            throw new SQLException();
+        }
+        return categoriaEncontrada;
+    }
+
+    public void eliminarCategoria(int id) throws SQLException {
+        PreparedStatement ps = null;
+
+        try {
+
+            Conexion_db objCon = new Conexion_db();
+            Connection conn = objCon.getConexion();
+            ps = conn.prepareStatement("DELETE FROM categoria WHERE id= '" + id + "'");
+            ps.setInt(1, id);
+            ps.execute();
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
             throw new SQLException();
